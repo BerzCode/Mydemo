b037b812d3b9ddbd926d8108b800bb317a74510d
Saurabh Kirve <32577373+BerzCode@users.noreply.github.com>
2023-01-19 12:42:27 +0530

    Done by my side
    Hello Team

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author skirv
+ *
+ */
+module Mydemo {
+}
---
+++ src/project/NewDemo.java
@@ -0,0 +1,10 @@
+package project;
+
+public class NewDemo {
+
+	public static void main(String[] args) {
+
+System.out.println("Hello");
+	}
+
+}
